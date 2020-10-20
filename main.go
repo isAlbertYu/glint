d8809794d7f979ee6eb2daff7b6afa57f5371c85
isAlbertYu <iameinstein24@qq.com>
2020-10-20 17:17:37 +0800

    测试demo

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 		v1.GET("/", func(c *glint.Context) {
 			c.HTML(http.StatusOK, "<h1>Hello Glint</h1>")
 		})
-		v1.GET("/hello", func(c *glint.Context) {
-			c.String(http.StatusOK, "hello %s, this is %s\n", c.Query("name"), c.Path)
-		})
+		//v1.GET("/hello", func(c *glint.Context) {
+		//	c.String(http.StatusOK, "hello %s, this is %s\n", c.Query("name"), c.Path)
+		//})
 	}
 	v2 := glint.NewGroup("/v2")
 	{
@@ -31,8 +31,7 @@ func main() {
 				"password": c.PostForm("password"),
 			})
 		})
-
 	}
-
+	// curl localhost:9999/v2/login -X POST -d "username=world&password=123"
 	myEngine.Run(":9999")
 }
